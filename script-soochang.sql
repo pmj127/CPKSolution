8a1211c72b0e6ea3fafec4bd42f78dd93ef11754
Soochang <soochang99@gmail.com>
2017-03-28 11:06:39 -0400

    User Lis script is updated

--- script-soochang.sql
+++ script-soochang.sql
@@ -1,6 +1,6 @@
 USE [CPKSolution]
 GO
-/****** Object:  StoredProcedure [CPK].[uspUserList]    Script Date: 2017-03-25 4:48:54 PM ******/
+/****** Object:  StoredProcedure [CPK].[uspUserList]    Script Date: 2017-03-28 11:00:46 AM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -10,22 +10,26 @@ GO
 -- Create date: <March 25, 2017>
 -- Description:	<>
 -- =============================================
-CREATE PROCEDURE [CPK].[uspUserList] 
+ALTER PROCEDURE [CPK].[uspUserList] 
 	@PageNo INT = 1,
-	@PageSize INT = 10
+	@PageSize INT = 10,
+	@FullName NVARCHAR(50)
 AS
 BEGIN
 	SET NOCOUNT ON;
 
     SELECT COUNT(*) AS TotalRows, @PageNo AS PageNo, @PageSize AS PageSize
-	FROM [CPK].[Users];	
+	FROM [CPK].[Users]
+	WHERE (FullName LIKE '%'+@FullName+'%' OR @FullName IS NULL OR LEN(@FullName) = 0);
 
 	SELECT UserID, FullName, Status, FORMAT(ModifyDate, 'MMM dd yyyy') AS ModifyDate
 	FROM [CPK].[Users]
+	WHERE (FullName LIKE '%'+@FullName+'%' OR @FullName IS NULL OR LEN(@FullName) = 0)
 	ORDER BY UserID DESC
 		OFFSET(@PageNo - 1) * @PageSize ROWS FETCH NEXT @PageSize ROWS ONLY
 	OPTION (OPTIMIZE FOR(@PageNo = 1, @PageSize = 10));
 END
+
 
 GO
 /****** Object:  StoredProcedure [CPK].[uspRegister]    Script Date: 2017-03-25 5:01:56 PM ******/
